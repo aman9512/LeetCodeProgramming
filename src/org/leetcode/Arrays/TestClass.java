e0b4dc70c6fd16d08b0116d00471773cb21c3c81
aman <aman@192.168.1.21>
2018-03-03 13:49:38 -0800

    first push

---
+++ src/org/leetcode/Arrays/TestClass.java
@@ -0,0 +1,9 @@
+package org.leetcode.Arrays;
+
+public class TestClass {
+
+	
+	public static void main (String[] args){
+		System.out.print("Hi Java");
+	}
+}
---
+++ src/org/leetcode/String/TestClass.java
@@ -0,0 +1,5 @@
+package org.leetcode.String;
+
+public class TestClass {
+	
+}
